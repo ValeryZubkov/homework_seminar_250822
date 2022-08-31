4b1a5f77c10ceb9ae464f3251b414d8378d66148
Valery Zubkov <bouddha@mail.ru>
2022-08-31 17:38:47 +0400

    homework number 3

---
+++ HOMEWORK_003/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+void FillMatrix(int[,] matr)
+{
+    for(int i=0; i<matr.GetLength(0); i++)
+    {
+        for(int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+void PrintMatrix(int[,] matr)
+{
+   for(int i=0; i<matr.GetLength(0); i++)
+    {
+        for(int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+Console.WriteLine("Введите количество строк m массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов n массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[m,n];
+FillMatrix(matrix);
+Console.WriteLine("Ваш массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+int temp = 0;
+int i = 0;
+for(int j=0; j<n; j++)
+{
+temp = matrix[i,j];
+matrix[i,j] = matrix[m-i-1,j];
+matrix[m-i-1,j] = temp;
+}
+PrintMatrix(matrix);
